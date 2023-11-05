4f026b31bf026f84e6b3ad0116fbad902c7ae745
SageCat <sagecatbox@gmail.com>
2023-11-05 03:54:33 +0200

    Refactor lib dir. Add SimpleDataProcessor and tests

--- lib/DataProcessorFactory.cpp
+++ lib/DataProcessorFactory.cpp
@@ -1,16 +1,21 @@
 #include "DataProcessorFactory.hpp"
 
+#include "DataProcessors/SimpleDataProcessor.hpp"
+
 namespace SageDocs
 {
     std::unique_ptr<IDataProcessor> DataProcessorFactory::createDataProcessor(DocType type)
     {
         switch (type)
         {
-        // TODO
-        // case DocType::SIMPLE_TABLE:
+        case DocType::SIMPLE_TABLE:
+            std::make_unique<SimpleDataProcessor>();
+            break;
         // case DocType::BESUGLAYA_INVOICE:
         default:
             throw std::invalid_argument("Unknown DocType provided");
         }
+
+        return nullptr;
     }
 }
