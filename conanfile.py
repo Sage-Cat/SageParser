9db873eaa36f1f4b3cc8ffc61f5cb5a251c9354f
SageCat <sagecatbox@gmail.com>
2024-01-03 17:40:37 +0200

    Fixed testSageDocsLib

--- conanfile.py
+++ conanfile.py
@@ -15,5 +15,4 @@ class SageDocsPkg(ConanFile):
         self.requires("rapidcsv/[8.80]")
         self.requires("csvmonkey/[0.0.5]")
         self.requires("pugixml/[1.14]")
-        self.requires("xlnt/[1.5.0]")
         self.requires("gtest/[1.14.0]")
