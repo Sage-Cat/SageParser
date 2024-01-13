748686f21679c5aa87ace2a7993d41be19ed0160
DanyloPlotskykh <145497398+DanyloPlotskykh@users.noreply.github.com>
2024-01-14 01:01:25 +0200

    Delete build directory

--- build/tests/CMakeFiles/TestSageDocsLib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TestSageDocsLib.
-# This may be replaced when dependencies are built.
--- build/ui/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/project/SageDocs/ui
-# Build directory: C:/project/SageDocs/build/ui
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
